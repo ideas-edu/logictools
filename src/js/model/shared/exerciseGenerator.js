519517b5c0b9221e98a270003f070ed6864c1468
Ben de Vries <mail@bendevries.com>
2021-03-09 10:01:03 +0100

    Added shared model that was forgotten in earlier commits

---
+++ src/js/model/shared/exerciseGenerator.js
@@ -0,0 +1,65 @@
+import { Resources } from '../../resources.js'
+import { LogEXSession } from '../../logEXSession.js'
+import { IdeasServiceProxy } from '../ideasServiceProxy.js'
+
+// Abstract class for generator
+export class ExerciseGenerator {
+  constructor () {
+    this.Exercise = undefined
+  }
+
+  /**
+        Generates a one way exercise.
+        @param {string} ExerciseType - Type of exercise to be generated.  See exerciseTypes.js for the full list
+        @param onExerciseGenerated - The callback function that is called after the exercise is generated.
+        The callback function expects 1 parameter, the exercise.
+        @param onErrorGeneratingExercise - The callback function that is called if there is a problem generating the exercise.
+     */
+  generate (exerciseType, properties, onExerciseGenerated, onErrorGeneratingExercise) {
+    const exerciseId = Resources.getExerciseMethod(exerciseType)
+    const userId = LogEXSession.getStudentId()
+    const difficulty = LogEXSession.getDifficulty()
+    const onError = onErrorGeneratingExercise
+    const onSuccess = function (data) {
+      this.onSuccess(data.generate, exerciseId, onErrorGeneratingExercise, onExerciseGenerated, properties)
+    }.bind(this)
+
+    IdeasServiceProxy.generate(exerciseId, difficulty, userId, onSuccess, onError)
+  }
+
+  example (exerciseNr, exerciseType, properties, onExerciseGenerated, onErrorGeneratingExercise) {
+    const exerciseId = Resources.getExerciseMethod(exerciseType)
+    const userId = LogEXSession.getStudentId()
+    const onError = onErrorGeneratingExercise
+    const onSuccess = function (data) {
+      this.onSuccess(data.example, exerciseId, onErrorGeneratingExercise, onExerciseGenerated, properties)
+    }.bind(this)
+
+    IdeasServiceProxy.example(exerciseId, exerciseNr, userId, onSuccess, onError)
+  }
+
+  /**
+        Creates an exercise based on input from the user.
+        @param onExerciseCreated - The callback function that is called after the exercise is created.
+        @param onErrorCreatingExercise - The callback function that is called if there is a problem creating the exercise.
+     */
+  create (exerciseId, formula, properties, onExerciseGenerated, onErrorGeneratingExercise) {
+    const userId = LogEXSession.getStudentId()
+    const onError = onErrorGeneratingExercise
+    const onSuccess = function (data) {
+      this.onSuccess(data.create, exerciseId, onErrorGeneratingExercise, onExerciseGenerated, properties)
+    }.bind(this)
+
+    IdeasServiceProxy.create(exerciseId, formula, userId, onSuccess, onError)
+  }
+
+  onSuccess (result, exerciseId, onErrorGeneratingExercise, onExerciseGenerated, properties) {
+    if (result.state === null || result.state.context.term === null) {
+      onErrorGeneratingExercise()
+    } else {
+      LogEXSession.setIdentifiers(exerciseId, result.state)
+      const exercise = new this.Exercise(result.state.context.term, exerciseId, properties)
+      onExerciseGenerated(exercise)
+    }
+  }
+}
---
+++ src/js/model/shared/exerciseSolver.js
@@ -0,0 +1,130 @@
+import { IdeasServiceProxy } from '../ideasServiceProxy.js'
+
+/**
+    ExerciseSolver is an abstract class for solvers
+    @constructor
+ */
+export class ExerciseSolver {
+  constructor () {
+    this.Step = undefined
+    this.StepCollection = undefined
+  }
+
+  /**
+        Solves a exercise.
+        @param {OneWayExercise} exercise - The exercise that needs to be solved.
+        @param onExerciseSolved - The callback function that is called after the exercise is solved.
+        The callback function expects 1 parameter, a ProofStepCollection.
+        @param onErrorSolvingExercise - The callback function that is called if there is a problem solving the exercise.
+     */
+  solve (exercise, onExerciseSolved, onErrorSolvingExercise) {
+    const onError = onErrorSolvingExercise
+    const onSuccess = function (data) {
+      if (data === null || data.error !== undefined || data.derivation.derivation.derivationsteps === null) {
+        onErrorSolvingExercise()
+        return
+      }
+      const steps = new this.StepCollection(new this.Step(exercise.formula, null))
+      const ds = data.derivation.derivation.derivationsteps
+      for (const i in ds) {
+        steps.push(new this.Step(ds[i].context.term, ds[i].step.rule))
+      }
+      onExerciseSolved(steps)
+    }.bind(this)
+
+    const state = this._getState(exercise)
+
+    IdeasServiceProxy.derivation(state, onSuccess, onError)
+  }
+
+  /**
+        Solves the next step of an exercise.
+        @param {OneWayExercise} exercise - the exercise of which the next step must be solved.
+        @param onNextStepSolved - The callback function that is called after the next step is solved.
+        The callback function expects 1 parameter, a OneWayStep.
+        @param onErrorSolvingNextStep - The callback function that is called if there is a problem solving the next step.
+     */
+  solveNextStep (exercise, onNextStepSolved, onErrorSolvingNextStep) {
+    const onError = onErrorSolvingNextStep
+    const onSuccess = function (data) {
+      if (data === null || data.error !== undefined || data.onefirst === null) {
+        if (data.error.search(/No step/i) >= 0) {
+          onErrorSolvingNextStep('no-step-possible')
+        } else {
+          onErrorSolvingNextStep('error-solving-next-step')
+        }
+        return
+      }
+
+      if (data.onefirst.length === 0) {
+        onErrorSolvingNextStep('error-solving-last-step')
+        return
+      }
+      const result = data.onefirst.first
+      const nextStep = new this.Step(result.state.context.term, result.step.rule)
+      if (nextStep) {
+        onNextStepSolved(nextStep)
+      }
+    }.bind(this)
+
+    const state = this._getState(exercise)
+
+    IdeasServiceProxy.onefirst(state, 'nextStep', onSuccess, onError)
+  }
+
+  /**
+        Gets the help for the next step of an exercise.
+        @param {OneWayExercise} exercise - The exercise of which the hint for the next step must be given.
+        @param onHelpForNextStepFound - The callback function that is called after the help for the next step is found.
+        The callback function expects 1 parameter, a OneWayStep.
+        @param onErrorGettingHelpForNextStep - The callback function that is called if there is a problem getting help for the next step.
+     */
+  getHelpForNextStep (exercise, onHelpForNextStepFound, onErrorGettingHelpForNextStep) {
+    const onError = onErrorGettingHelpForNextStep
+    const onSuccess = function (data) {
+      if (data === null || data.error !== undefined || data.onefirst === null) {
+        if (data.error.search(/No step/i) >= 0) {
+          onErrorGettingHelpForNextStep('no-step-possible')
+        } else {
+          onErrorGettingHelpForNextStep('error-showing-hint')
+        }
+      } else {
+        const currentStep = exercise.steps.getCurrentStep()
+        const onewaySteps = new this.StepCollection(new this.Step(currentStep.formula, null))
+        const result = data.onefirst.first
+        onewaySteps.push(new this.Step(result.state.context.term, result.step.rule))
+        onHelpForNextStepFound(onewaySteps.getCurrentStep())
+      }
+    }.bind(this)
+
+    const state = this._getState(exercise)
+
+    IdeasServiceProxy.onefirst(state, 'Hint: useRule', onSuccess, onError)
+  }
+
+  _getState (exercise) {
+    const currentStep = exercise.steps.getCurrentStep()
+    let currentStrategy
+    let currentFormula
+
+    if (currentStep === null) {
+      currentFormula = exercise.formula
+      currentStrategy = '[]'
+    } else {
+      currentFormula = currentStep.formula
+      currentStrategy = currentStep.strategyStatus
+    }
+
+    const state = {
+      exerciseid: exercise.type,
+      prefix: currentStrategy,
+      context: {
+        term: currentFormula,
+        environment: {},
+        location: []
+      }
+    }
+
+    return state
+  }
+}
