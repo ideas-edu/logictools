b0b8eb131923047ab8a1cbbed6a9763792374ccd
Ben de Vries <mail@bendevries.com>
2021-04-12 14:51:10 +0200

    Manual entry works (#70)

--- src/shared/kbinput/kbinput.js
+++ src/shared/kbinput/kbinput.js
@@ -47,19 +47,21 @@ export class FormulaPopover {
       button.addEventListener('mousedown', this.addText.bind(this))
     }
     // Undo button
-    const unButton = document.createElement('button')
-    unButton.type = 'button'
-    unButton.innerHTML = '&#9100;'
-    unButton.classList = 'btn btn-sm btn-outline-secondary'
+    if (this.options.allowUndo) {
+      const unButton = document.createElement('button')
+      unButton.type = 'button'
+      unButton.innerHTML = '&#9100;'
+      unButton.classList = 'btn btn-sm btn-outline-secondary'
 
-    this.wrapper.appendChild(unButton)
-    unButton.addEventListener('mousedown', function () {
-      this.undo()
-      // Keep focus on inputElement after pressing button
-      window.setTimeout(() => {
-        this.inputElement.focus()
-      }, 1)
-    }.bind(this))
+      this.wrapper.appendChild(unButton)
+      unButton.addEventListener('mousedown', function () {
+        this.undo()
+        // Keep focus on inputElement after pressing button
+        window.setTimeout(() => {
+          this.inputElement.focus()
+        }, 1)
+      }.bind(this))
+    }
 
     // Backspace button
     const bsButton = document.createElement('button')
