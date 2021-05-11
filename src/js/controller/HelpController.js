135bd974af8adb711f1590aeff0853bab0e44613
Ben de Vries <mail@bendevries.com>
2021-05-11 13:53:58 +0200

    Fixed linter error and added config to tests

--- src/js/controller/HelpController.js
+++ src/js/controller/HelpController.js
@@ -4,5 +4,5 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'katex/dist/katex.min.css'
 
 window.translate = () => {
-	// do nothing, all translation is done by the updateTexts function in mainFrameController.js
+  // do nothing, all translation is done by the updateTexts function in mainFrameController.js
 }
