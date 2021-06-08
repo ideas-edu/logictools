58cd5e448fbc9b636a90f5ef3a16282a5666dc09
Ben de Vries <mail@bendevries.com>
2021-06-08 17:39:34 +0200

    Added step history and highlighting changes in LogAx steps

--- src/js/model/logax/stepCollection.js
+++ src/js/model/logax/stepCollection.js
@@ -1,5 +1,5 @@
 import { StepCollection } from '../shared/stepCollection.js'
-
+import { LogAxStep } from './step.js'
 /**
     LogAxStepCollection is an ordered list of conversion steps.
     @constructor
@@ -12,6 +12,8 @@ export class LogAxStepCollection extends StepCollection {
     if (baseStep !== null && baseStep !== undefined) {
       this.push(baseStep)
     }
+    this.stepsHistory = [JSON.parse(JSON.stringify(this.steps))]
+    this.stepsHistoryIndex = 0
   }
 
   /**
@@ -21,5 +23,34 @@ export class LogAxStepCollection extends StepCollection {
   push (step) {
     this.steps.push(step)
     this.steps.sort((a, b) => a.number > b.number)
+  }
+
+  newSet (steps) {
+    this.steps = []
+    for (const responseStep of steps) {
+      const newStep = new LogAxStep(responseStep)
+      this.steps.push(newStep)
+      // highlight differences
+      let found = false
+      for (const oldStep of this.stepsHistory[this.stepsHistoryIndex]) {
+        if (newStep.number === oldStep.number) {
+          if (newStep.rule !== oldStep.rule) {
+            newStep.highlightRule = true
+          }
+          if (newStep.term !== oldStep.term) {
+            newStep.highlightTerm = true
+          }
+          found = true
+          break
+        }
+      }
+      if (!found) {
+        newStep.highlightStep = true
+        newStep.highlightRule = true
+        newStep.highlightTerm = true
+      }
+    }
+    this.stepsHistory.push(JSON.parse(JSON.stringify(this.steps)))
+    this.stepsHistoryIndex += 1
   }
 }
