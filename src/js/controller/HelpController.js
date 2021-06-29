25c500a6e1875191fc15a3fe64b1f2a5ad895d17
Ben de Vries <mail@bendevries.com>
2021-06-29 16:37:21 +0200

    Further delay loading video

--- src/js/controller/HelpController.js
+++ src/js/controller/HelpController.js
@@ -16,17 +16,19 @@ function ready (fn) {
 }
 
 function setUp () {
-  document.querySelectorAll('video').forEach(item => {
-    if (item.getAttribute('_src') !== '') {
-      const sourceMp4 = document.createElement('source')
-      sourceMp4.setAttribute('src', `${item.getAttribute('_src')}.mp4`)
-      item.appendChild(sourceMp4)
-      const sourceWebm = document.createElement('source')
-      sourceWebm.setAttribute('src', `${item.getAttribute('_src')}.webm`)
-      item.appendChild(sourceWebm)
-      item.load()
-    }
-  })
+  setTimeout(function () {
+    document.querySelectorAll('video').forEach(item => {
+      if (item.getAttribute('_src') !== '') {
+        const sourceMp4 = document.createElement('source')
+        sourceMp4.setAttribute('src', `${item.getAttribute('_src')}.mp4`)
+        item.appendChild(sourceMp4)
+        const sourceWebm = document.createElement('source')
+        sourceWebm.setAttribute('src', `${item.getAttribute('_src')}.webm`)
+        item.appendChild(sourceWebm)
+        item.load()
+      }
+    })
+  }, 1000)
 }
 
 ready(setUp)
