43bef2acb71fe48b93e6a7ff0b23082e85748323
Ben de Vries <mail@bendevries.com>
2021-08-19 09:52:42 +0200

    Added help videos for LogAx

--- src/js/controller/HelpController.js
+++ src/js/controller/HelpController.js
@@ -7,28 +7,28 @@ window.translate = () => {
   // do nothing, all translation is done by the updateTexts function in mainFrameController.js
 }
 
-function ready (fn) {
-  if (document.readyState !== 'loading') {
-    fn()
-  } else {
-    document.addEventListener('DOMContentLoaded', fn)
-  }
-}
+// function ready (fn) {
+//   if (document.readyState !== 'loading') {
+//     fn()
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn)
+//   }
+// }
 
-function setUp () {
-  setTimeout(function () {
-    document.querySelectorAll('video').forEach(item => {
-      if (item.getAttribute('_src') !== '') {
-        const sourceMp4 = document.createElement('source')
-        sourceMp4.setAttribute('src', `${item.getAttribute('_src')}.mp4`)
-        item.appendChild(sourceMp4)
-        const sourceWebm = document.createElement('source')
-        sourceWebm.setAttribute('src', `${item.getAttribute('_src')}.webm`)
-        item.appendChild(sourceWebm)
-        item.load()
-      }
-    })
-  }, 1000)
-}
+// function setUp () {
+//   setTimeout(function () {
+//     document.querySelectorAll('video').forEach(item => {
+//       if (item.getAttribute('_src') !== '') {
+//         const sourceMp4 = document.createElement('source')
+//         sourceMp4.setAttribute('src', `${item.getAttribute('_src')}.mp4`)
+//         item.appendChild(sourceMp4)
+//         const sourceWebm = document.createElement('source')
+//         sourceWebm.setAttribute('src', `${item.getAttribute('_src')}.webm`)
+//         item.appendChild(sourceWebm)
+//         item.load()
+//       }
+//     })
+//   }, 1000)
+// }
 
-ready(setUp)
+// ready(setUp)
